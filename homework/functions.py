3fc661198a250ea1d12dfa5a7a69af98d5c97e60
sobandom <sobandom@unal.edu.co>
2024-12-14 10:13:21 -0500

    taller 14 - predictiva

--- homework/functions.py
+++ homework/functions.py
@@ -163,3 +163,63 @@ def save_metrics(metrics):
         metrics = saved_metrics
 
     metrics.to_csv("../files/output/metrics.csv", index=False)
+
+def compute_evaluation_metrics(df, y_true_column="yt_true"):
+    """Compute metrics for train and test sets"""
+
+    cols = [col for col in df.columns if "yt_pred" in col]
+
+    y_train_true = df[y_true_column].iloc[:-24]
+    y_test_true = df[y_true_column].iloc[-24:]
+
+    results = {}
+    results["Metrics"] = ["MSE Train", "MSE Test", "MAE Train", "MAE Test"]
+
+    for col in cols:
+
+        y_train_pred = df[col].iloc[:-24]
+        y_test_pred = df[col].iloc[-24:]
+
+        metrics = [
+            mean_squared_error(y_train_true, y_train_pred),
+            mean_squared_error(y_test_true, y_test_pred),
+            mean_absolute_error(y_train_true, y_train_pred),
+            mean_absolute_error(y_test_true, y_test_pred),
+        ]
+
+        results[col] = metrics
+
+    results = pd.DataFrame(results)
+
+    results = results.round(2)
+
+    return results
+
+def add_sin_cos_components(df):
+
+    df = df.assign(sin_12m=np.sin(2 * np.pi * df.month / 12))
+    df = df.assign(cos_12m=np.cos(2 * np.pi * df.month / 12))
+
+    df = df.assign(sin_6m=np.sin(2 * np.pi * df.month / 6))
+    df = df.assign(cos_6m=np.cos(2 * np.pi * df.month / 6))
+
+    df = df.assign(sin_4m=np.sin(2 * np.pi * df.month / 4))
+    df = df.assign(cos_4m=np.cos(2 * np.pi * df.month / 4))
+
+    df = df.assign(sin_3m=np.sin(2 * np.pi * df.month / 3))
+    df = df.assign(cos_3m=np.cos(2 * np.pi * df.month / 3))
+
+    return df
+
+def make_lagged_ts(df, p_max, y_column, fmt="lagged_{}m"):
+    for i in range(1, p_max + 1):
+        df[fmt.format(i)] = df[y_column].shift(i)
+    return df
+
+def make_yt_true_scaled(df, scaler):
+    df["yt_true_scaled"] = scaler.fit_transform(df[["yt_true"]])
+    return df
+
+def remove_trend_and_cycle(df, yt_true_name="yt_true"):
+    df[yt_true_name + "_d1d12"] = df[yt_true_name].diff(1).diff(12)
+    return df
